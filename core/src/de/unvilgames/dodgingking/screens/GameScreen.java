17f5a966c62b7a5e88ac1183245720c739fb8c2f
mit1234123 <tim.j.k@hotmail.de>
2017-08-04 00:32:59 +0200

    Intoducing the bases where player stands.added basic player/base sprites

--- core/src/de/unvilgames/dodgingking/screens/GameScreen.java
+++ core/src/de/unvilgames/dodgingking/screens/GameScreen.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import de.unvilgames.dodgingking.DodgingKing;
 import de.unvilgames.dodgingking.Resources;
 import de.unvilgames.dodgingking.graph.Background;
+import de.unvilgames.dodgingking.graph.SizeEvaluator;
 
 /**
  * Created by timjk on 29.07.2017.
@@ -23,6 +24,11 @@ public class GameScreen extends DefaultScreen {
     public static final int SCREEN_W = 12 * Resources.TILE_SIZE; // 192
     public static final int SCREEN_H = 8 * Resources.TILE_SIZE; // 128
 
+    public static final int MAX_BASE_X = 3;
+    public static final int MAX_BASE_Y = 3;
+
+    private SizeEvaluator sizeEvaluator;
+
     private Stage gameStage;
 
     private Background bg;
@@ -34,10 +40,26 @@ public class GameScreen extends DefaultScreen {
         ExtendViewport viewport = new ExtendViewport(SCREEN_W, SCREEN_H);
         gameStage = new Stage(viewport, batch);
         bg = new Background();
+        sizeEvaluator = new SizeEvaluator(gameStage, game.res, MAX_BASE_X, MAX_BASE_Y);
     }
 
     public void update(float delta) {
         gameStage.act(delta);
+    }
+
+    public void drawBases() {
+        batch.begin();
+
+        //draw 4x4 bases
+        for(int x = 0; x <= MAX_BASE_X; x++) {
+            for(int y = 0; y <= MAX_BASE_Y; y++) {
+                batch.draw(game.res.base, sizeEvaluator.getBaseScreenX(x), sizeEvaluator.getBaseScreenY(y));
+            }
+        }
+
+        batch.draw(game.res.player, sizeEvaluator.getBaseScreenX(1), sizeEvaluator.getBaseScreenY(1) + sizeEvaluator.BASE_MARGIN);
+
+        batch.end();
     }
 
     @Override
@@ -49,6 +71,8 @@ public class GameScreen extends DefaultScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         bg.draw(gameStage, game.res);
+        drawBases();
+
         gameStage.draw();
     }
 
